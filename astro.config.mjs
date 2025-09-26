6e4ad8326992765724411166142720c3762595a1
Brayan M. Beltre <brayanmbeltre@gmail.com>
2025-09-26 13:31:59 -0400

    feat: added redirect to figma portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,4 +12,7 @@ export default defineConfig({
       applyBaseStyles: false,
     }),
   ],
+    redirects: {
+    "/portfolio": "https://www.figma.com/deck/3LVtaaODfk7NuuoMMJxxYI/Kenny-Beltre-s-Portfolio?node-id=1-2495&viewport=-6753%2C-138%2C0.76&t=YpCfOBNok5ukzM4i-1&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1",
+  }
 });
